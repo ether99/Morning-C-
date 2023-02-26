8ff48254cfc30ed5ab762c501c9683e7dc93a120
ether99 <ether8@yandex.ru>
2023-02-26 16:49:18 +0300

    Task 1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -60,3 +60,24 @@
 
     //   453216 -> 32
 
+
+// HomeWork1
+// Task1. Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+
+  Console.Write("The first number " + num1 + 
+  " is greater than the second number " + num2);
+else
+  Console.Write("The second number " + num2 + 
+  " is greater than the first number " +  num1);
+  
