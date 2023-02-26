8a7815fb3cf275bb47d91f37170aa4aec2a102ad
ether99 <ether8@yandex.ru>
2023-02-26 19:11:57 +0300

    Task3

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -80,7 +80,7 @@ if (num1 > num2)
 else
   Console.Write("The second number " + num2 + 
   " is greater than the first number " +  num1);
-*/
+
 
   // Task 2. Напишите программу, которая принимает на вход три числа 
   //         и выдаёт максимальное из этих чисел.
@@ -102,5 +102,22 @@ else
   if (num3 > max)
     max = num3;
   Console.Write(" Max number: " + max);
- 
+ */
+
+ // Task 3. Напишите программу, которая на вход принимает число и выдаёт, 
+ //         является ли число чётным (делится ли оно на два без остатка).
+//          4 -> да
+//          -3 -> нет
+//          7 -> нет
   
+
+  Console.Write("Enter a number: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+  Console.Write("The number: " + num + " is even.");
+}
+else
+{
+  Console.Write(" Tne number: " + num + " is not even.");
+}
