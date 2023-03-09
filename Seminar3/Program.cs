48579bad085036c6d0d1de3b8b3843d8c266ea52
ether99 <ether8@yandex.ru>
2023-03-10 01:32:36 +0300

    Homework3

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -33,7 +33,7 @@
 
   //   int quarter = getQuarter(x, y);
   //     if (quarter > 0)
-  //         Console.WriteLine($"point is in (quarter) quarter");
+  //         Console.WriteLine($"point is in {quarter} quarter");
   //         else
   //         Console.WriteLine($"point is not quarters");
 
@@ -71,3 +71,83 @@
     //       squareTable(n);
     //       else
     //       Console.Write("Введите число больше 0");
+
+
+// Homework 3:
+// Task 1: 
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+  // Decision:
+  // Console.WriteLine("Input a number. ");
+  //   string number = Console.ReadLine();
+  //   int ar = number.Length; 
+  //     if(ar == 5) {
+  //       if (number[0] == number[4] && number[1] == number[3]) {
+  //           Console.WriteLine($"{number} is a palindrome.");
+  //       }
+  //       else {
+  //         Console.WriteLine($"{number} is not a palindrome. ");
+  //       }
+  //     }
+  //     else {
+  //       Console.WriteLine($"The number: {number} is not five - digit.");
+  //     }
+
+// Task 2:
+// Напишите программу, которая принимает на вход координаты двух точек и находит 
+//  расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Decision:
+/*
+    int x1 = ReadInt("Enter the coordinate X the first point: ");
+    int y1 = ReadInt("Enter the coordinate Y the first point:  ");
+    int z1 = ReadInt("Enter the coordinate Z the first point:  ");
+    int x2 = ReadInt("Enter the coordinate X the second point: : ");
+    int y2 = ReadInt("Enter the coordinate Y the second point: : ");
+    int z2 = ReadInt("Enter the coordinate Z the second point: : ");
+
+    int A = x2 - x1;
+    int B = y2 - y1;
+    int C = z1 - z2;
+
+      double length = Math.Sqrt(A * A + B * B + C * C);
+      
+      Console.WriteLine($"distance {length}");
+
+      int ReadInt(string message)
+      {
+          Console.Write(message);
+          return Convert.ToInt32(Console.ReadLine());
+      }
+
+*/
+
+// Task 3:
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Decision:
+/*
+ void squareTable(int n) {
+    int currentNumber = 1;
+     while(currentNumber <= n) {
+        Console.WriteLine(currentNumber + " - " + (currentNumber * currentNumber * currentNumber));
+    currentNumber++;
+      }
+    }
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+          if (n > 0)
+          squareTable(n);
+          else
+          Console.Write("Enter a positive number.");
+*/
+
+   
