73a219ad10ea18ce931ac88fa16c7303fb151e8d
ether99 <ether8@yandex.ru>
2023-03-14 01:53:46 +0300

    Homework4

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,91 +1,92 @@
-﻿/*
-Task1:
-int GetSum(int a)
-{
-int sum = 0;
+﻿
 
-for (int current = 1; current <= a; current++)  
+// Homework 4:
 
-    sum += current;
-    return sum;
+//     Task 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//             3, 5 -> 243 (3⁵)
+//             2, 4 -> 16
 
-}
-Console.Write("Input a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+   /* int numberA = ReadInt("Enter a number: ");
+    int numberB = ReadInt("Enter the gegree: ");
+        Degree(numberA, numberB);
 
-    int result = GetSum(number);
-    Console.WriteLine($"Sum of numbers from 1 to {number} is {result}");
-*/
-
-// Task1: Напишите программу, которая принимает на вход число и выдает количество 
-//        цифр в числе.
-
-    /*int Digits(int num)
-    {
-        int digits = 0;
-        if (num == 0) digits = 1;
-        while (num != 0)
-        {
-            num /= 10;
-            digits++;
-        }
-
-        return digits;
-    }
-
-        Console.Write("Input your number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        int dig = Digits(number);
-
-        Console.WriteLine($"There are {dig} digits in {number}");
- */
-
-
-
-
-// Task3: Напишите программу которая принимает на вход число N и выдает произведение
-//        чисел от 1 до N;
-/*
-int Factorial(int num)
-{
+        void Degree(int a, int b)
+  {
     int result = 1;
-    for (int current = 1; current <= 0; current++)
+        for (int i = 1; i <= b; i++)
     {
-        result *= current;
+         result = result * a;
     }
+    Console.WriteLine(a + " in degree " + b + " =  " + result);
+  }
 
-        return result;
-}
-    Console.Write("Enter your number: ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    int result = Factorial(number);
-    Console.WriteLine($"Factorial of number {number} is {result}");
+
+        int ReadInt(string message)
+  {
+        Console.WriteLine(message);
+        return Convert.ToInt32(Console.ReadLine());
+  }
+
 */
-/*
-int [] CreateRandomArray(int size, int minValue, int maxValue)
-{
-    int[] array = new int[size];
-    for(int i = 0; i < size; i++) 
-        array[i] = new Random().Next(minValue, maxValue + 1);
-    return array;
-}
-    void ShowArray(int[] array)
-    {
-        for(int i = 0; i < array.Length; i++)
-        
-            Console.Write(array[i] + " ");
-            Console.WriteLine();
-      
-    }
-    Console.Write("Input quantity of elements: ");
-    int size = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a min possible value: ");
-    int min = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a max possible value: ");
-    int max = Convert.ToInt32(Console.ReadLine());
-
-    int[] newArray = CreateRandomArray(size, min, max);
-    ShowArray(newArray);
-    */
-
     
+
+
+
+// Task 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//         452 -> 11
+//         82 -> 10
+//         9012 -> 12
+/*
+    int number = ReadInt("Enter a number: ");
+
+    int len = NumberLen(number);
+    SumNumbers(number, len);
+    int NumberLen(int a)
+    {
+        int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+    void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"sum of digits {sum}");
+}
+
+    int ReadInt(string message)
+{
+     Console.Write(message);
+       return Convert.ToInt32(Console.ReadLine());
+}
+*/
+
+// Task 3: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+//         1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//         6, 1, 33 -> [6, 1, 33]
+
+/*
+   int lenArray = ReadInt("Enter the length of the array: ");
+
+    int[] randomArray = new int[lenArray];
+      for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
