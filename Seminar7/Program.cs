aa887e9d07bcefbba8bf7946b7e0f218a479a4b2
ether99 <ether8@yandex.ru>
2023-03-23 23:26:33 +0300

    HomeWork7

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,35 +1,35 @@
-﻿int [,] CreateRandom2dArray()
-{
-  //int [,] array = new int [rows, columns];
+﻿// int [,] CreateRandom2dArray()
+// {
+//   //int [,] array = new int [rows, columns];
 
-    Console.Write("Input a number of rows:");
-    int rows = Convert. ToInt32(Console. ReadLine ( ));
-    Console.Write("Input a number of columns:");
-    int columns = Convert. ToInt32(Console. ReadLine ( ));
-    Console.Write("Input a min possible value:");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-    Console.Write ("Input a max possible value:");
-    int maxvalue = Convert. ToInt32(Console. ReadLine ()) ;
+//     Console.Write("Input a number of rows:");
+//     int rows = Convert. ToInt32(Console. ReadLine ( ));
+//     Console.Write("Input a number of columns:");
+//     int columns = Convert. ToInt32(Console. ReadLine ( ));
+//     Console.Write("Input a min possible value:");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.Write ("Input a max possible value:");
+//     int maxvalue = Convert. ToInt32(Console. ReadLine ()) ;
 
-  for(i = 0; i < rows; i++)
-    for(int j = 0; j < columns; j++ )
-    array[i,j] = newRandom().Next(minValue, maxValue);
+//   for(i = 0; i < rows; i++)
+//     for(int j = 0; j < columns; j++ )
+//     array[i,j] = newRandom().Next(minValue, maxValue);
 
-    return array;
-}
+//     return array;
+// }
 
-void Show2dArray(int[,] array)
-{
-  for(int i = 0; i < array.GetLength(0); i++)
-  {
-    for(int j = 0; j < array.Get.Length(1); j++)
-    Console.Write(array[i,j] + " ");
+// void Show2dArray(int[,] array)
+// {
+//   for(int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for(int j = 0; j < array.Get.Length(1); j++)
+//     Console.Write(array[i,j] + " ");
 
-    Console.WriteLine();
-  }
-  Console.WriteLine();
-}
+//     Console.WriteLine();
+//   }
+//   Console.WriteLine();
+// }
 
 
-Sit[,] newArray = CreateRandom2dArray (rows, columns, minvalue, maxValue);
-ShowdArray (newArray);
+// Sit[,] newArray = CreateRandom2dArray (rows, columns, minvalue, maxValue);
+// ShowdArray (newArray);
